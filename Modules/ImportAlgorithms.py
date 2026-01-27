dc5f54bc826d65cdf4e07d70d445d3af84a167f6
Billapink <sibyllensheikh@outlook.com>
2026-01-27 14:25:07 +0000

    Added changes to the Import Module

--- Modules/ImportAlgorithms.py
+++ Modules/ImportAlgorithms.py
@@ -1,7 +1,10 @@
 import csv
 import pandas as pd
+
 class ImportAlgorithms:
     def __init__(self, transaction_repo):
+        #passing in the transaction repository and defining required columns
+        # for financial transaction data
         self.transaction_repo = transaction_repo
         self.required_columns = ["date", "merchant", "amount", "description"]
 
@@ -9,8 +12,20 @@ class ImportAlgorithms:
         transactions_dict = {}
         headers = []
         values=[]
+        #reading the csv file 
         with open(csv_file, 'r') as csv:
             csv_reader= csv.reader(csv)
 
+        #populating the headers array with the transaction data fields
         headers = next(csv_reader)
+        #populating the transaction dictionary with the headers and an 
+        # array of their corresponding values
+        for header in headers:
+            for row in csv_reader:
+                values.append(row[header])
+                transactions_dict.update({"%s", values})
+                values= []
+
+        #parsing this dictionary into a dataframe for easier use
+        dataframe = pd.DataFrame(transactions_dict)
 
