bdd2b480245cf479eba9f0e1c241faa52a7eaf0c
Billapink <sibyllensheikh@outlook.com>
2026-03-30 01:55:01 +0100

    Deleted old rows from transactions table

--- server/Data/createCsv.py
+++ server/Data/createCsv.py
@@ -4,14 +4,22 @@ import pandas as pd
 
 fake = Faker()
 
-merchants = ['ALIBABA.COM', 'PACKAGING SUPPLIES DIRECT', 'DHL EXPRESS', 'META ADS', 'GOOGLE ADS', 'TIKTOK ADS', 'SHELL', 'REGUS', 'BRITISH GAS', 'STRIPE PAYMENTS', 'SHOPIFY PAYOUT', 'MICROSOFT 365', 'ADOBE CREATIVE CLOUD', 'SLACK', 'ROYAL MAIL', 'STAPLES', 'HMRC PAYE', 'GUSTO', 'HMRC VAT', 'AMAZON', 'UBER']
+merchants = ['ALIBABA.COM', 'PACKAGING SUPPLIES DIRECT', 'DHL EXPRESS', 'META ADS', 'GOOGLE ADS', 'TIKTOK ADS', 'SHELL', 'REGUS', 'BRITISH GAS', 'STRIPE PAYMENTS', 'SHOPIFY PAYOUT', 'MICROSOFT 365', 'ADOBE CREATIVE CLOUD', 'SLACK', 'ROYAL MAIL', 'STAPLES', 'HMRC PAYE', 'GUSTO', 'HMRC VAT', 'AMAZON', 'UBER', 'SALES']
 descriptions = ['']
 
 print('Date,Merchant,Amount')
 rows = []
-for i in range(20):
+for i in range(1000):
+    if i % 7 == 0:
+        rows.append({
+        "Date": fake.date_between(start_date=date(2020,1,1),end_date=date.today()),
+        "Merchant": fake.random_element(merchants),
+        "Amount": fake.random_int(1, 1000000000)/100,
+        "Description": fake.random_element(descriptions)
+    })
+
     rows.append({
-        "Date": fake.date_between(start_date=date(2025,1,1),end_date=date.today()),
+        "Date": fake.date_between(start_date=date(2020,1,1),end_date=date.today()),
         "Merchant": fake.random_element(merchants),
         "Amount": fake.random_int(1, 10000000)/100,
         "Description": fake.random_element(descriptions)
